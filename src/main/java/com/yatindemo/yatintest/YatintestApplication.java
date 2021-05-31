b87574d51b87de4468123ece09d72125786c5997
sajjadamu <hussain.net17@gmail.com>
2021-05-31 17:51:57 +0530

    Initial comment

---
+++ src/main/java/com/yatindemo/yatintest/YatintestApplication.java
@@ -0,0 +1,13 @@
+package com.yatindemo.yatintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatintestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatintestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yatindemo/yatintest/YatintestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yatindemo.yatintest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YatintestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
